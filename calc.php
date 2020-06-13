daece0106868622a08566b2662ed3bd899828323
WrongUse <56023131+WrongUse@users.noreply.github.com>
2020-06-13 18:37:26 +0300

    Update calc.php

--- calc.php
+++ calc.php
@@ -1,23 +1,28 @@
 <?php
 if (!empty($_POST)) {
- $x = !empty($_POST['x']) ? $_POST['x'] : '';
- $y = !empty($_POST['y']) ? $_POST['y'] : '';
- $operation = !empty($_POST['operation']) ?
- $_POST['operation']
- : '';
- if (!is_int($x) || !is_int($y)) {
- echo 'Неверный тип данных';
- } else {
- switch ($operation) {
- case 'sum':
- $result = $x.'+'.$y.'=' . ($x + $y);
- break;
- case 'diff':
- $result = $x.'-'.$y.'=' . ($x - $y);
- break;
- default:
- $result = 'Передан неизвестный тип операции';
- }
- echo $result;
- }
+    $x = isset($_POST['x']) ? $_POST['x'] : '';
+    $y = isset($_POST['y']) ? $_POST['y'] : '';
+    $operation = !empty($_POST['operation']) ?
+        $_POST['operation']        : '';
+    switch ($operation) {
+        case 'sum':
+            $result = $x . '+' . $y . '=' . ($x + $y);
+            break;
+        case 'diff':
+            $result = $x . '-' . $y . '=' . ($x - $y);
+            break;
+        case '*':
+            $result = $x . '*' . $y . '=' . ($x * $y);
+            break;
+        case '/':
+            if ($y == 0) {
+                $result = 'Нельзя делить на ноль';
+                break;
+            }
+            $result = $x . '/' . $y . '=' . ($x / $y);
+            break;
+        default:
+            $result = 'Передан неизвестный тип операции';
+    }
+    echo $result;
 }
